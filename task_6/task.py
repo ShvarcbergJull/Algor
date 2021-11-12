6691596a618c3a0fe1b07fc84bee942a60707522
Юлия Шварцберг <yulia2399@mail.ru>
2021-11-12 22:37:59 +0300

    mur

--- task_6/task.py
+++ task_6/task.py
@@ -19,18 +19,6 @@ def generate_table(count_vert, count_edge):
             i += 1
     return table
 
-def generate_grid(n, m, count_walls):
-    table = np.ones((n, m))
-    i = 0
-    while i <= count_walls:
-        gor = random.randint(0, n - 1)
-        vert =  random.randint(0, m - 1)
-        if table[gor][vert] != 0:
-            table[gor][vert] = 0
-            i += 1
-        
-    return table
-
 if __name__ == "__main__":
     count_vert = 100
     count_edge = 500
@@ -58,35 +46,6 @@ if __name__ == "__main__":
 
     task1_result = task1(graph)
     print(task1_result)
-
-
-    grid = generate_grid(10, 20, 40)
-    nodes = {}
-    edgs = []
-
-    a = 0
-    b = 0
-    i = 0
-
-    for a in range(10):
-        for b in range(20):
-            if grid[a][b] == 0:
-                continue
-            if a == 0 and b == 19:
-                source = i
-            if a == 9 and b == 0:
-                target = i
-            nodes[i] = (a, b)
-            i += 1
-
-    for nd1 in nodes.keys():
-        for nd2 in nodes.keys():
-            if nd1 == nd2:
-                continue
-            if nodes[nd1][0] == nodes[nd1][0] and abs(nodes[nd1][1] - nodes[nd2][1]) == 1:
-                edgs.append((nd1, nd2))
-            if nodes[nd1][1] == nodes[nd1][1] and abs(nodes[nd1][0] - nodes[nd2][0]) == 1:
-                edgs.append((nd1, nd2))
 
     grph2 =  nx.generators.lattice.grid_graph([20,10])
     i = 0
